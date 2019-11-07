ad5de7299ddd799130bb7d451d596230c0e0264d
Wojciech Tałałaj <wojtal.pl@gmail.com>
2019-11-07 18:04:56 +0100

    Adapted bindings to RxStores 0.1.0

--- src/useStore.tsx
+++ src/useStore.tsx
@@ -1,31 +1,23 @@
 import { useEffect, useState } from 'react';
 import { Provider, Store, StoreClass, StoreInterface, StoreModel } from '@leocode/rxstores';
-import { SubscriptionLike } from 'rxjs';
 
 export function useStore<T extends Store>(
     storeImplementation: StoreClass<T>,
     context?: string,
-): [StoreModel<T> | undefined, StoreInterface<T> | undefined] {
-    const [data, setData] = useState<StoreModel<T>>();
-    const [methods, setMethods] = useState<StoreInterface<T>>();
-    const [subscription, setSubscription] = useState<SubscriptionLike>();
+): [StoreModel<T>, StoreInterface<T>] {
+    const store = context
+        ? Provider.from(context).getStore(storeImplementation)
+        : Provider.getStore(storeImplementation);
 
-    async function bindToStore() {
-        const store = await (context
-            ? Provider.from(context).getStore(storeImplementation)
-            : Provider.getStore(storeImplementation));
-        setData(store.value);
-        setMethods(store.methods);
-        setSubscription(store.data$.subscribe(setData));
-    }
+    const [data, setData] = useState<StoreModel<T>>(store.value);
 
     useEffect(() => {
-        bindToStore();
+        const subscription = store.data$.subscribe(setData);
         return () => subscription && subscription.unsubscribe();
     }, []);
 
     return [
         data,
-        methods
+        store.methods
     ];
 }
